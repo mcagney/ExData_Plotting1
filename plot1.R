2c00bf3bfda0c55e4de69dd0e10f87f3c87df538
mcagney <megan.cagney@yahoo.com>
2015-06-04 16:09:26 -0400

    Initial Commit of Plot 1 R code

---
+++ plot1.R
@@ -0,0 +1,12 @@
+## Get Data. File saved to my computer. 
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Prep the Data 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Make the Plot
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
